fe4e889a712c9e955b07f4608490be717948c191
Julia Hoffman <julia.hoffman@blueyonder.co.uk>
2015-11-12 14:34:49 +0000

    nitrous-backup at Thu Nov 12 14:34:49 UTC 2015

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,17 +15,45 @@ WIN_COMBINATIONS = [
   [2,4,6]
   ]
 
+
+
 def won?(board)
-  WIN_COMBINATIONS.each do |win_array|
-    if [Board].any?{|win_array|} == true
-      puts "Winning array"
-      #won = true
-      return [Board].select{|win_array|}
-      #WIN_COMBINATIONS.each do |combination|
-    else false
-      Puts "Not a win"
-    end
+  if board[0] = "X" && board[1] = "X" && board[2] = "X"
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    else
+    false
   end
+  #WIN_COMBINATIONS.select do |win_array|
+#    win_array.board
+
+  #if board[0]= 'X' && board[1] = 'X' && board[2] = 'X'
+    #return WIN_COMBINATIONS[0]
+  #else false
+  #end
+#top_row_win = WIN_COMBINATIONS[0]
+#mid_row_win = WIN_COMBINATIONS[1]
+#bottom_row_win = WIN_COMBINATIONS[2]
+#left_column_win = WIN_COMBINATIONS[3]
+#mid_column_win = WIN_COMBINATIONS[4]
+#right_column_win = WIN_COMBINATIONS[5]
+#diagonal_win_1 = WIN_COMBINATIONS[6]
+#diagonal_win_2 = WIN_COMBINATIONS[7]
+
+#  position_1 = board[win_index_1]
+ # [board].select do |win|
+#    win.WIN_COMBINATIONS
+#  else false
+ # end
+ # WIN_COMBINATIONS.each do |win_array|
+   # if [Board].any?{|win_array|} == true
+  #    puts "Winning array"
+      #won = true
+ #     return [Board].select{|win_array|}
+      #WIN_COMBINATIONS.each do |combination|
+ #   else false
+   #   Puts "Not a win"
+  #  end
+#  end
 end
 
 def full?(board)
